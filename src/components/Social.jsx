8b34b0fc8b6c3ecc12c193f9442e4f347e806b55
roodhouse <rughjm@gmail.com>
2023-07-12 12:52:20 -0500

    mbView done

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Facebook from '../images/icon-facebook.svg'
+import Pinterest from '../images/icon-pinterest.svg'
+import Instagram from '../images/icon-instagram.svg'
 
 function Social() {
   return (
-    <div>Social</div>
+    <>
+      <div id="socialContainer" className='bg-[url("/src/images/bg-hills.svg")] h-[168px] flex pl-[119px] pt-[96px] pb-12'>
+        <div id="facebookIcon" className='mr-[34px]'>
+          <img src={Facebook} alt="Facebook" />
+        </div>
+        <div id="pinterestIcon" className='mr-[34px]'>
+          <img src={Pinterest} alt="Pinterest" />
+        </div>
+        <div id="instagramIcon">
+          <img src={Instagram} alt="Instagram" className=''/>
+        </div>
+      </div>
+    </>
   )
 }
 
