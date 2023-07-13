d24fd9d6fd4af9cbf2f4e1c075833a19bbaf9b98
roodhouse <rughjm@gmail.com>
2023-07-13 15:33:59 -0500

    max width etc

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,15 +6,17 @@ import Instagram from '../images/icon-instagram.svg'
 function Social() {
   return (
     <>
-      <div id="socialContainer" className='bg-[url("/src/images/bg-hills.svg")] h-[168px] flex pl-[119px] pt-[96px] pb-12'>
-        <div id="facebookIcon" className='mr-[34px]'>
-          <img src={Facebook} alt="Facebook" />
-        </div>
-        <div id="pinterestIcon" className='mr-[34px]'>
-          <img src={Pinterest} alt="Pinterest" />
-        </div>
-        <div id="instagramIcon">
-          <img src={Instagram} alt="Instagram" className=''/>
+      <div id="socialContainer" className='bg-[url("/src/images/bg-hills.svg")] h-[168px] flex justify-center pl-0 pt-[96px] pb-12'>
+        <div id="moreContain" className='flex'>
+          <div id="facebookIcon" className='mr-[34px]'>
+            <img src={Facebook} alt="Facebook" />
+          </div>
+          <div id="pinterestIcon" className='mr-[34px]'>
+            <img src={Pinterest} alt="Pinterest" />
+          </div>
+          <div id="instagramIcon">
+            <img src={Instagram} alt="Instagram" className=''/>
+          </div>
         </div>
       </div>
     </>
